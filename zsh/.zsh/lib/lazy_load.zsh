39cb3d2a068435b62c5c9e3edb4d57c9212ceab0
phar-dev <pablo.hv.2002@gmail.com>
2026-01-16 23:57:17 -0600

    Refactor lazy loading to direct module loading in lazy_load.zsh

--- zsh/.zsh/lib/lazy_load.zsh
+++ zsh/.zsh/lib/lazy_load.zsh
@@ -1,169 +1,21 @@
 #-----------------------------------------
-# LAZY LOADING DE HERRAMIENTAS
+# CARGA DIRECTA DE HERRAMIENTAS
 #-----------------------------------------
-# Este archivo contiene las funciones de lazy loading para cargar herramientas solo cuando se usan.
-# Usamos funciones stub que cargan el módulo la primera vez que se ejecutan.
+# Este archivo carga los módulos directamente sin lazy loading.
 
-# Lazy loading para herramientas de desarrollo
-function lg() {
-  if [[ -z "$DEV_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/dev.zsh"
-    export DEV_LOADED=1
-  fi
-  unfunction lg
-  lg "$@"
-}
-function pg() {
-  if [[ -z "$DEV_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/dev.zsh"
-    export DEV_LOADED=1
-  fi
-  unfunction pg
-  pg "$@"
-}
-function my() {
-  if [[ -z "$DEV_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/dev.zsh"
-    export DEV_LOADED=1
-  fi
-  unfunction my
-  my "$@"
-}
+# Carga directa para herramientas de desarrollo
+load_module "$ZSH_TOOLS_DIR/dev.zsh"
 
-# Lazy loading para PHP/Laravel
-function php() {
-  if [[ -z "$PHP_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/php.zsh"
-    export PHP_LOADED=1
-  fi
-  unfunction php
-  php "$@"
-}
-function composer() {
-  if [[ -z "$PHP_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/php.zsh"
-    export PHP_LOADED=1
-  fi
-  unfunction composer
-  composer "$@"
-}
-function pa() {
-  if [[ -z "$PHP_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/php.zsh"
-    export PHP_LOADED=1
-  fi
-  unfunction pa
-  pa "$@"
-}
-function sail() {
-  if [[ -z "$PHP_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/php.zsh"
-    export PHP_LOADED=1
-  fi
-  unfunction sail
-  sail "$@"
-}
-function pint() {
-  if [[ -z "$PHP_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/php.zsh"
-    export PHP_LOADED=1
-  fi
-  unfunction pint
-  pint "$@"
-}
+# Carga directa para PHP/Laravel
+load_module "$ZSH_TOOLS_DIR/php.zsh"
 
-# Lazy loading para Node.js
-function node() {
-  if [[ -z "$NODE_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/node.zsh"
-    export NODE_LOADED=1
-  fi
-  unfunction node
-  node "$@"
-}
-function npm() {
-  if [[ -z "$NODE_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/node.zsh"
-    export NODE_LOADED=1
-  fi
-  unfunction npm
-  npm "$@"
-}
-function pnpm() {
-  if [[ -z "$NODE_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/node.zsh"
-    export NODE_LOADED=1
-  fi
-  unfunction pnpm
-  pnpm "$@"
-}
-function bun() {
-  if [[ -z "$NODE_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/node.zsh"
-    export NODE_LOADED=1
-  fi
-  unfunction bun
-  bun "$@"
-}
-function pn() {
-  if [[ -z "$NODE_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/node.zsh"
-    export NODE_LOADED=1
-  fi
-  unfunction pn
-  pn "$@"
-}
-function nr() {
-  if [[ -z "$NODE_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/node.zsh"
-    export NODE_LOADED=1
-  fi
-  unfunction nr
-  nr "$@"
-}
-function nx() {
-  if [[ -z "$NODE_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/node.zsh"
-    export NODE_LOADED=1
-  fi
-  unfunction nx
-  nx "$@"
-}
-function fnm() {
-  if [[ -z "$NODE_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/node.zsh"
-    export NODE_LOADED=1
-  fi
-  unfunction fnm
-  fnm "$@"
-}
+# Carga directa para Node.js
+load_module "$ZSH_TOOLS_DIR/node.zsh"
 
-# Lazy loading para Go
-function go() {
-  if [[ -z "$GO_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/golang.zsh"
-    export GO_LOADED=1
-  fi
-  unfunction go
-  go "$@"
-}
+# Carga directa para Go
+load_module "$ZSH_TOOLS_DIR/golang.zsh"
 
-# Lazy loading para Docker
-function docker() {
-  if [[ -z "$DOCKER_LOADED" ]]; then
-    load_module "$ZSH_TOOLS_DIR/docker.zsh"
-    export DOCKER_LOADED=1
-  fi
-  unfunction docker
-  docker "$@"
-}
+# Carga directa para Docker
+load_module "$ZSH_TOOLS_DIR/docker.zsh"
 
-# Agrega tus lazy loads personalizados aquí
-# Ejemplo:
-# function lazy_load_custom() {
-#   if [[ "$1" =~ ^(comando) ]]; then
-#     # Cargar módulo o configurar
-#     add-zsh-hook -d preexec lazy_load_custom
-#   fi
-# }
-# add-zsh-hook preexec lazy_load_custom
+# Agrega tus cargas personalizadas aquí si es necesario
