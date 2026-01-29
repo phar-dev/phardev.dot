e75ab6c2c1c231f3353d9d1f6596fc3221869165
phar-dev <phar-dev@github.com>
2026-01-29 10:55:01 -0600

    Update Fish aliases function

--- fish/.config/fish/functions/aliases.fish
+++ fish/.config/fish/functions/aliases.fish
@@ -3,14 +3,18 @@
 #-----------------------------------------
 # Aliases para navegación y listado de directorios
 # Utiliza 'lsd' como reemplazo mejorado del comando 'ls' estándar
-alias ls='lsd'                    # Mostrar listado básico con lsd (alternativa moderna a ls)
-alias la='ls -a'                  # Mostrar archivos y directorios ocultos (incluyendo . y ..)
-alias lla='ls -la'                # Mostrar listado detallado incluyendo archivos ocultos
-alias lt='ls --tree'              # Mostrar estructura de directorios en formato árbol
+alias ls='lsd' # Mostrar listado básico con lsd (alternativa moderna a ls)
+alias la='ls -a' # Mostrar archivos y directorios ocultos (incluyendo . y ..)
+alias lla='ls -la' # Mostrar listado detallado incluyendo archivos ocultos
+alias lt='ls --tree' # Mostrar estructura de directorios en formato árbol
 
 # Aliases para desarrollo y editores
-alias n='nvim .'                  # Abrir Neovim en el directorio actual
-alias pj='cd ~/Projects && ls'    # Navegar al directorio de proyectos y listar su contenido
-alias vc='code --reuse-window .'  # Abrir VSCode en la ventana actual para el directorio actual
+alias n='nvim .' # Abrir Neovim en el directorio actual
+alias pj='cd ~/Projects && ls' # Navegar al directorio de proyectos y listar su contenido
+alias vc='code --reuse-window .' # Abrir VSCode en la ventana actual para el directorio actual
 
 alias oc='opencode'
+
+alias sail='./vendor/bin/sail'
+alias sup='./vendor/bin/sail up -d & ./vendor/bin/sail pnpm dev'
+
